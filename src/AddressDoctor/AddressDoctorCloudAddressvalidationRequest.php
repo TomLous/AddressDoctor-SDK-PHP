1c76ed3a94a1db0f21bae659fddc786c3faa9de3
tomlous <tomlous@gmail.com>
2015-06-02 11:39:58 +0200

    error?

--- src/AddressDoctor/AddressDoctorCloudAddressvalidationRequest.php
+++ src/AddressDoctor/AddressDoctorCloudAddressvalidationRequest.php
@@ -9,22 +9,63 @@
 namespace AddressDoctor;
 
 
-class AddressDoctorCloudAddressvalidationRequest
+class AddressDoctorCloudAddressValidationRequest
 {
 
     const VERSION = '2';
 
-    const BASE_WEBSERVICE_URL = 'http://validator5.AddressDoctor.com/Webservice5/v2/addressvalidation.asmx';
+    const WEBSERVICE_URL = 'http://validator5.AddressDoctor.com/Webservice5/v2/addressvalidation.asmx';
 
-    const WDSL_URL = 'http://validator5.addressdoctor.com/webservice5/v2/addressvalidation.asmx?WSDL';
+    const WSDL_URL = 'http://validator5.addressdoctor.com/webservice5/v2/addressvalidation.asmx?WSDL';
+
+    private $soapClient;
+
+    private $login;
+    private $password;
+
+    const PROCESS_MODE_INTERACTIVE = 'INTERACTIVE';
+    const PROCESS_MODE_FASTCOMPLETION = 'FASTCOMPLETION';
+    const PROCESS_MODE_BATCH = 'BATCH';
+    const PROCESS_MODE_CERTIFIED = 'CERTIFIED';
+    const PROCESS_MODE_ADDRESSCODELOOKUP = 'ADDRESSCODELOOKUP';
 
 
-    public function __construct()
+
+    public function __construct($login, $password)
     {
 
-        $client = new SoapClient("some.wsdl", array('soap_version' => SOAP_1_2));
+        $this->soapClient = new SoapClient(self::WSDL_URL, array('soap_version' => SOAP_1_2));
+
+        $this->login = $login;
+        $this->password = $password;
+
+
 
     }
 
 
-} 
+    public function test(){
+        $this->process(self::PROCESS_MODE_INTERACTIVE);
+    }
+
+
+    public function process($processMode, $jobToken=null, $campaignId=null, $reservedXml=null, $useTransactionPool=null){
+
+        $serviceParameters = new ServiceParameters($jobToken, $campaignId, $reservedXml, $useTransactionPool);
+        $validationParameters = new ValidationParameters();
+
+        $parameters = new Parameters($processMode, $serviceParameters, $validationParameters);
+        $parameters->setProcessMode($processMode);
+
+
+        $addresses = new ArrayOfAddress();
+        $enrichments =  new ArrayOfEnrichment();
+
+        $process = new Process($this->login, $this->password, $parameters, $addresses, $enrichments);
+
+
+    }
+
+
+}
+
